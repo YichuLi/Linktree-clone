35b3491c91ceaf497401737e824c99134105e15e
YichuLi <yichu.li@outlook.com>
2023-04-29 19:59:13 -0400

    add link to database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,85 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import supabase from '../../utils/supabaseClient'
+import { useEffect, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [userID, setUserID] = useState<string | undefined>();
+  const [title, setTitle] = useState<string | undefined>();
+  const [url, setUrl] = useState<string | undefined>();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await supabase.auth.getUser();
+      console.log("user", user); 
+      if ((await user).data) {
+        const userID = user.data.user?.id;
+        setIsAuthenticated(true);
+        setUserID(userID);
+      }
+      console.log("isAuthenticated: ", isAuthenticated);
+    };
+    getUser();  // need to call the function
+  }, []);
+
+  const addNewLink = async () => {
+    try {
+      if (title && url && userID) {
+        const { data, error } = await supabase.from('links').insert({
+          title: title,
+          url: url,
+          user_id: userID
+        }).select();
+        if (error) throw error;
+        console.log("data: ", data)
+      }
+    }
+    catch (error) {
+      console.log("error: ", error)
+    }
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col w-full justify-center items-center mt-4">  
+      {
+        isAuthenticated && (
+          <>
+          <div className="mt-4">
+              <div className="block text-sm font-medium text-gray-700">
+                    Title
+              </div>
+              <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="my awesome link"
+                  onChange={(e) => setTitle(e.target.value)}
+              />
+          </div>
+          <div className="mt-4">
+              <div className="block text-sm font-medium text-gray-700">
+                URL
+              </div>
+              <input
+                  type="text"
+                  name="url"
+                  id="url"
+                  className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="https://github.com/YichuLi"
+                  onChange={(e) => setUrl(e.target.value)}
+              />
+          </div>
+          <button
+            type="button"
+            className="mt-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+            onClick={addNewLink}
+          >
+            Add new link
+          </button>
+          </>
+        )
+      }
+    </div>
   )
 }
